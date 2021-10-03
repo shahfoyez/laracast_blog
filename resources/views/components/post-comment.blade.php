44234c65838f1ff7bfb1c2654d249be6a1c81bd0
Shah Foyez <shahfoyez7@gmail.com>
2021-10-03 15:24:14 +0600

    Comment Update

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,14 @@
+<article class='flex bg-gray-100 boarder boarder-gray-200 p-6 rounded-xl space-x-4'>
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" width="60" height="60" class="rounded-xl"/>
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class='font-bold'>
+                John Doe
+            </h3>
+            <time>6 Months Ago</time>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quidem dolorem iure provident.</p>
+        </header>
+    </div>
+</article>
